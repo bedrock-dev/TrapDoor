634856c51d7038b405194f35dd67b1f97a066d7c
OEOTYAN <1124718975@qq.com>
2021-01-21 21:38:53 +0800

    fix rotate

--- api/entity/PlayerBuffer.cpp
+++ api/entity/PlayerBuffer.cpp
@@ -5,16 +5,13 @@
 #include "PlayerBuffer.h"
 
 namespace trapdoor {
-    bool RightClickCache::operator==(const RightClickCache &rhs) const {
-        if (!(x == rhs.x &&
-              y == rhs.y &&
-              z == rhs.z))
-            return false;
-        auto distance2 = (dx - rhs.dx) * (dx - rhs.dx) + (dy - rhs.dy) * (dy - rhs.dy) + (dz - rhs.dz) * (dz - rhs.dz);
-        return distance2 < 0.01f;
-    }
+	bool RightClickCache::operator==(const RightClickCache& rhs) const {
+		if (!(x == rhs.x && y == rhs.y && z == rhs.z))
+			return false;
+		return (gameTick - rhs.gameTick) < 4;
+	}
 
-    bool RightClickCache::operator!=(const RightClickCache &rhs) const {
-        return !(rhs == *this);
-    }
-}
+	bool RightClickCache::operator!=(const RightClickCache& rhs) const {
+		return !(rhs == *this);
+	}
+}  // namespace trapdoor
