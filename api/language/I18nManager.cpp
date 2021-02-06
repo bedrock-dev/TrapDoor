eee002a45ea8f90e837fb6172b54a6cb0c436f2c
hhhxiao <1417599491@qq.com>
2021-02-06 15:37:55 +0800

    add simple i18n

---
+++ api/language/I18nManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xhy on 2021/2/5.
+//
+
+#include "I18nManager.h"
---
+++ api/language/I18nManager.h
@@ -0,0 +1,14 @@
+//
+// Created by xhy on 2021/2/5.
+//
+
+#ifndef MOD_I18NMANAGER_H
+#define MOD_I18NMANAGER_H
+
+
+class I18nManager {
+
+};
+
+
+#endif //MOD_I18NMANAGER_H
