064a9cc7700ee9e8cf23791e3d6c80f1c2e839a6
hhhxiao <1417599491@qq.com>
2021-03-23 13:43:37 +0800

    add auto inject and save output ps script

---
+++ inject.ps1
@@ -0,0 +1,16 @@
+chcp 65001
+$INJECTOR =  "MCDllInject.exe"
+$GAME_VERSION = "1.16.40.02"
+$BDS_DIR = "C:\Users\xhy\Desktop\MC\bedrock-server-"+$GAME_VERSION+"\"
+$TD_BUILD_DIR = "C:\Users\xhy\dev\TrapDoor\cmake-build-release\"
+$BDS_FILE = $BDS_DIR+"bedrock_server.exe"
+$LOG_DIR = "C:\Users\xhy\dev\TrapDoor\log\"
+$Date =  $(get-date -f yyyy-MM-dd)+" "+$(get-date -f HH-mm-ss)
+
+Write-Output "ues injector          "$INJECTOR
+Write-Output "game version is       "$GAME_VERSION
+Write-Output "bds location is       "$BDS_FILE
+Write-Output "trapdoor build dir is "$TD_BUILD_DIR
+
+Set-Location $BDS_DIR
+Invoke-Expression $INJECTOR" "$BDS_FILE" "$TD_BUILD_DIR | Tee-Object $LOG_DIR""$Date".txt"
